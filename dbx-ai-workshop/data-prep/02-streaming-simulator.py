5bcb23181085f6025b4d0f758e758f4058ce16ea
memomsft <guillermora@microsoft.com>
2025-09-29 11:28:23 -0600

    Update 02-streaming-simulator.py

--- dbx-ai-workshop/data-prep/02-streaming-simulator.py
+++ dbx-ai-workshop/data-prep/02-streaming-simulator.py
@@ -7,6 +7,12 @@ from random import randint, choice, uniform
 from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType, TimestampType
 from pyspark.sql import Row, functions as F
 
+catalog = "<your_catalog>"
+schema = "<your_schema>"
+
+
+spark.sql(f"USE CATALOG {catalog}")
+spark.sql(f"USE {catalog}.{schema}")
 
 orders_schema = StructType([
 StructField("order_id", IntegerType(), False),
@@ -23,9 +29,9 @@ statuses = ["Pending", "Shipped", "Cancelled"]
 
 base_id = int(time.time())
 for i in range(20):
-new = [(base_id + i, randint(1,5), randint(1,5), round(uniform(50,1500),2), choice(statuses))]
-df = spark.createDataFrame(new, schema=StructType(orders_schema[:-1]))
-df = df.withColumn("order_ts", F.current_timestamp())
-df.write.mode("append").saveAsTable("orders")
-print(f"Inserted order {base_id + i}")
-time.sleep(5)
+    new = [(base_id + i, randint(1,5), randint(1,5), round(uniform(50,1500),2), choice(statuses))]
+    df = spark.createDataFrame(new, schema=StructType(orders_schema[:-1]))
+    df = df.withColumn("order_ts", F.current_timestamp())
+    df.write.mode("append").saveAsTable("orders")
+    print(f"Inserted order {base_id + i}")
+    time.sleep(5)
